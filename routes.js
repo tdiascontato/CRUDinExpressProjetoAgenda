be68caf6cecba70f170a7222383ee24897a18aa4
Tiago Dias <98658691+tdiascontato@users.noreply.github.com>
2023-04-09 14:15:17 -0300

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const route = express.Router();
+const {middlewareGlobal, checkCsrfError} = require('./src/middlewares/middleware.js');
+const homeController = require('./src/Controllers/homeController');
+const loginController = require('./src/Controllers/loginController');
+route.get('/', homeController.index);
+route.get('/login/index', loginController.index);
+module.exports = route;
